c281c1c4e5df4cee249dd898db3d436ba67a0ea7
Prasamsha <prasamsha.dotel@danphecare.com>
2025-10-09 15:21:28 +0545

    (Fix) - Some changes in css

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,26 +4,26 @@ import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
 import hero_image from '../Assets/hero_image.png'
 export const Hero = () => {
-  return (
-    <div className='hero'>
-        <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
-            <div>
-                <div className="hero-hand-icon">
-                    <p>new</p>
-                    <img src={hand_icon}alt=""/>
+    return (
+        <div className='hero'>
+            <div className="hero-left">
+                <h2>NEW ARRIVALS ONLY</h2>
+                <div>
+                    <div className="hero-hand-icon">
+                        <p>New</p>
+                        <img src={hand_icon} alt="" />
+                    </div>
+                    <p>Collection</p>
+                    <p>For Everyone</p>
                 </div>
-                <p>Collection</p>
-                <p>for everyone</p>
+                <div className="hero-latest-btn">
+                    <div>Latest Collection</div>
+                    <img src={arrow_icon} alt=""></img>
+                </div>
             </div>
-            <div className="hero-latest-btn">
-                <div>Latest Collection</div>
-                <img src={arrow_icon} alt=""></img>
+            <div className="hero-right">
+                <img src={hero_image} alt=""></img>
             </div>
         </div>
-        <div className="hero-right">
-            <img src={hero_image} alt=""></img>
-        </div>
-    </div>
-  )
+    )
 }
